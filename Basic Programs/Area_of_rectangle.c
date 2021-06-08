cfd886e313b16cf626e500be1c3cc4b52b14a3e9
Indrajeet-Gharge <85387813+Indrajeet-Gharge@users.noreply.github.com>
2021-06-08 10:32:07 +0530

    Create Area_of_rectangle.c

---
+++ Basic Programs/Area_of_rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+    int l,b;
+    printf("\n Enter Two Numbers \n");
+    scanf("%d%d",&l,&b);
+    printf("Area of rectangle =%d\n",(l * b));
+    return 0;
+
+}
+
