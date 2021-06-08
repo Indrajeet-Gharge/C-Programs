8b0214e7e45e2fa3afc9159e4023276dbf42d182
Indrajeet-Gharge <85387813+Indrajeet-Gharge@users.noreply.github.com>
2021-06-08 10:17:05 +0530

    Add files via upload

---
+++ Addition.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main()
+{
+
+    int a,b,c;
+    printf("Enter values a and b=\n");
+    scanf("%d%d",&a,&b);
+    c = a + b;
+    printf("Addition of a and b =%d\n",c);
+    return 0;
+
+}
