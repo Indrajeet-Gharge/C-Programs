f17618bf0656c2409a40fab4602a61b92cfd4b54
Indrajeet-Gharge <85387813+Indrajeet-Gharge@users.noreply.github.com>
2021-06-10 11:00:44 +0530

    Add files via upload

---
+++ Basic Programs/Area_of_circle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+
+    float r, pi=3.14;
+
+    printf("\n Enter Radius \n");
+
+    printf("Enter radius of a circle:\n ");
+    scanf("%f",&r);
+
+    printf("\n Area \n");
+
+    printf("Area of circle: %f \n",(pi*r*r));
+
+    return 0;
+
+}
