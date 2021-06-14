149bdf216b23e0d42d9be5375ada657fbcf7caf0
Indrajeet-Gharge <85387813+Indrajeet-Gharge@users.noreply.github.com>
2021-06-14 11:35:27 +0530

    ASCII_Value_of_Character.c

---
+++ Basic Programs/ASCII_Value_of_Character.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{
+
+    char ch;
+    printf("\n Enter any character:");
+    scanf("%c" ,&ch);
+
+    printf(" \n ASCII value of character %c is: %d", ch , ch);
+
+    return 0;
+
+}
