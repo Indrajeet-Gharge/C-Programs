add7c8fc6596050efa1be09a7c0595822069d4e2
Indrajeet-Gharge <85387813+Indrajeet-Gharge@users.noreply.github.com>
2021-07-05 12:21:17 +0530

    inputing_String.c

---
+++ String Program.c/inputing_String.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     char st[20];
+
+     printf("Enter your name: ");
+     scanf("%s", st);
+
+     printf("Your name is %s.", st);
+
+     getch();
+     return 0;
+}
