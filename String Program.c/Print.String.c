e51d11356a82a27bd83eab6781b33675046484c0
Indrajeet-Gharge <85387813+Indrajeet-Gharge@users.noreply.github.com>
2021-07-06 10:55:18 +0530

    Print.String.c

---
+++ String Program.c/Print.String.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+     char str[] = "Virat";
+     char *ptr = str;
+
+     while(*ptr != '\0')
+     {
+         printf("%c", *ptr);
+         ptr++;
+     }
+
+     getch();
+     return 0;
+}
