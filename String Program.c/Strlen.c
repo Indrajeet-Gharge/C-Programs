40efb99746a6c1d9762f8e8bfb351f79db54a69f
Indrajeet-Gharge <85387813+Indrajeet-Gharge@users.noreply.github.com>
2021-07-11 10:43:40 +0530

    Add files via upload

---
+++ String Program.c/Strlen.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+    char *s = "Virat Kohli";
+    int a = strlen(s);
+
+    printf("The length of string s is %d", a);
+
+    getch();
+    return 0;
+}
